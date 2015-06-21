60911942d07960d30aff413b1ac43f025c1bcb02
datla.raja@gmail.com <datla.raja@gmail.com>
2015-06-21 18:38:49 +0100

    Latest changes.

--- user-management/src/com/kthree/controller/UserController.java
+++ user-management/src/com/kthree/controller/UserController.java
@@ -17,90 +17,79 @@ import com.kthree.dao.UserDao;
 import com.kthree.model.User;
 
 public class UserController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private static String INSERT_OR_EDIT = "/user.jsp";
-    private static String LIST_USER = "/listUser.jsp";
-    private UserDao dao;
+	private static final long serialVersionUID = 1L;
+	private static String INSERT_OR_EDIT = "/user.jsp";
+	private static String LIST_USER = "/listUser.jsp";
+	private UserDao dao;
 
-    public UserController() {
-        super();
-        dao = new UserDao();
-    }
+	public UserController() {
+		super();
+		dao = new UserDao();
+	}
 
-    @Override
-    public void init() throws ServletException {
-    	// TODO Auto-generated method stub
-    	super.init();
-    	System.out.println("Launching User controller. Please standby!");
-    }
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	Logger log = Logger.getLogger(UserController.class.getName());
-    	log.debug("Initiating logs....");
-    	log.info(getServletInfo());
-    	log.debug(getServletContext());
-    	log.error(getInitParameterNames());
-    	log.fatal(getServletConfig());
-    	log.warn(getServletConfig().getServletName());
-    	log.debug("Existing log.");
-    	log.trace(getServletName());
-    	String forward="";
-        String action = request.getParameter("action");
-        if (action.equalsIgnoreCase("delete")){
-            int userId = Integer.parseInt(request.getParameter("userId"));
-            dao.deleteUser(userId);
-            forward = LIST_USER;
-            request.setAttribute("users", dao.getAllUsers());    
-        } else if (action.equalsIgnoreCase("edit")){
-            forward = INSERT_OR_EDIT;
-            int userId = Integer.parseInt(request.getParameter("userId"));
-            User user = dao.getUserById(userId);
-            request.setAttribute("user", user);
-        } else if (action.equalsIgnoreCase("listUser")){
-            forward = LIST_USER;
-            request.setAttribute("users", dao.getAllUsers());
-        } else {
-            forward = INSERT_OR_EDIT;
-        }
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		System.out.println("Launching User controller. Please standby!");
+	}
 
-        RequestDispatcher view = request.getRequestDispatcher(forward);
-        view.forward(request, response);
-    }
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		Logger log = Logger.getLogger(UserController.class.getName());
+		log.debug("Initiating logs....");
+		log.info(getServletInfo());
+		log.debug(getServletContext());
+		log.error(getInitParameterNames());
+		log.fatal(getServletConfig());
+		log.warn(getServletConfig().getServletName());
+		log.debug("Existing log.");
+		log.trace(getServletName());
+		String forward = "";
+		String action = request.getParameter("action");
+		if (action.equalsIgnoreCase("delete")) {
+			int userId = Integer.parseInt(request.getParameter("userId"));
+			dao.deleteUser(userId);
+			forward = LIST_USER;
+			request.setAttribute("users", dao.getAllUsers());
+		} else if (action.equalsIgnoreCase("edit")) {
+			forward = INSERT_OR_EDIT;
+			int userId = Integer.parseInt(request.getParameter("userId"));
+			User user = dao.getUserById(userId);
+			request.setAttribute("user", user);
+		} else if (action.equalsIgnoreCase("listUser")) {
+			forward = LIST_USER;
+			request.setAttribute("users", dao.getAllUsers());
+		} else {
+			forward = INSERT_OR_EDIT;
+		}
 
-    public static void main(String args[]){
-    	Logger log = Logger.getLogger(UserController.class.getName());
-    	log.debug("Initiating logs....");
-    	log.info("INFO LOGS");
-    	log.debug("DEBUG LOG");
-    	log.error("ERROR LOG");
-    	log.fatal("FATAL LOG");
-    	log.warn("WARN LOG");
-    	log.trace("TRACE LOG");
-    	log.debug("Exiting logs.. Good bye!");
-    }
-    
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = new User();
-        user.setFirstName(request.getParameter("firstName"));
-        user.setLastName(request.getParameter("lastName"));
-        try {
-            Date dob = new SimpleDateFormat("MM/dd/yyyy").parse(request.getParameter("dob"));
-            user.setDob(dob);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        user.setEmail(request.getParameter("email"));
-        String userid = request.getParameter("userid");
-        if(userid == null || userid.isEmpty())
-        {
-            dao.addUser(user);
-        }
-        else
-        {
-            user.setUserid(Integer.parseInt(userid));
-            dao.updateUser(user);
-        }
-        RequestDispatcher view = request.getRequestDispatcher(LIST_USER);
-        request.setAttribute("users", dao.getAllUsers());
-        view.forward(request, response);
-    }
+		RequestDispatcher view = request.getRequestDispatcher(forward);
+		view.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		User user = new User();
+		user.setFirstName(request.getParameter("firstName"));
+		user.setLastName(request.getParameter("lastName"));
+		try {
+			Date dob = new SimpleDateFormat("MM/dd/yyyy").parse(request
+					.getParameter("dob"));
+			user.setDob(dob);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		user.setEmail(request.getParameter("email"));
+		String userid = request.getParameter("userid");
+		if (userid == null || userid.isEmpty()) {
+			dao.addUser(user);
+		} else {
+			user.setUserid(Integer.parseInt(userid));
+			dao.updateUser(user);
+		}
+		RequestDispatcher view = request.getRequestDispatcher(LIST_USER);
+		request.setAttribute("users", dao.getAllUsers());
+		view.forward(request, response);
+	}
 }
